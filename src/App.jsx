11ada791b5f9525388733f9d8e9d96aab8f0ddf5
Gaurav Thakur <gauravthakur2015.gt@gmail.com>
2023-09-26 17:30:03 +0530

    Feature: Preventing user from adding same person again

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-function App() {
-  
+import { useState } from 'react'
+import "./style.css"
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'John doe' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleClick = e => {
+    e.preventDefault()
+    const isPersonExist = persons.find(person => person.name.toLowerCase() === newName.toLowerCase())
+    if (isPersonExist)
+    {
+      alert(`${newName} is already added to the phonebook`)
+      return
+    }
+    const personToAdd = {name : newName}
+    setPersons([...persons, personToAdd])
+    setNewName('')
+  }
+
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
+        </div>
+        <div>
+          <button type="submit" onClick={handleClick}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map(person => <p key={person.name}>{person.name}</p> )
+      }
+    </div>
   )
 }
 
